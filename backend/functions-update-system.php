bc4ad373799be26d3432226e033b488952d8ffb5
clarelynton <clare.lynton@gmail.com>
2018-02-03 09:28:53 +1100

    Add files via upload

---
+++ backend/functions-update-system.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @package split
+ * @since split 1.0.0
+ * @license GPL 2.0
+ * 
+ */
+
+#-------------------------------------------------------------
+# Update System
+#-------------------------------------------------------------
+
+get_template_part( 'backend/functions-theme-update', 'checker' ); 
+get_template_part( 'backend/functions-theme-update', 'settings' ); 
---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @package split
+ * @since split 1.0.0
+ * @license GPL 2.0
+ *
+ */
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
